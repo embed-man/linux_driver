2e0dd49f7ed19ec80a116983dc6b19ab31466eb7
xuzhiyin <xzy@example.com>
2020-12-01 16:49:37 +0800

    更新

--- linux_4.15/simple_platform/platform_driver.c
+++ linux_4.15/simple_platform/platform_driver.c
@@ -3,41 +3,49 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/platform_device.h>
+#include <linux/mod_devicetable.h>
 #include <asm/io.h>
  
 static int beep_probe(struct platform_device *pdev)
 {
 	int ret;	
-	printk("driver probe ok!");
+	struct resource	*mem;
+    mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    
+	printk(KERN_ERR "platform_driver_probe driver probe ok!\n");
 	return 0;
 }
  
 static int beep_remove(struct platform_device *pdev)
 {
-
-	printk("driver remove ok!");
+    
+	printk(KERN_ERR "driver remove ok!");
 	return 0;
 }
  
- 
+static struct platform_device_id p_id = {
+    .name="bigbang",
+};
+
 static struct platform_driver beep_driver=
 {
     .driver.name = "bigbang",
-    .probe = beep_probe,
+    /*.probe = beep_probe,*/
     .remove = beep_remove,
+    .id_table = &p_id
 };
- 
  
 static int beep_init(void)
 {
-	printk("driver module_init");
+	printk(KERN_ERR "driver module_init\n");
 	
-	return platform_driver_register(&beep_driver);
+    return platform_driver_probe(&beep_driver, beep_probe);
+	//return platform_driver_register(&beep_driver);
 }
  
 static void beep_exit(void)
 {
-	printk("driver module_exit");
+	printk(KERN_ERR "driver module_exit\n");
 	platform_driver_unregister(&beep_driver);
 	
 	return;
